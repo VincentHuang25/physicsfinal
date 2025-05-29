a50f5baf76439da27b62d34bcf0406c57f7b92f5
Kellen Yu <80976955+ringedSquid@users.noreply.github.com>
2025-05-29 10:02:33 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+Web VPython 3.2
+
+class Inductor:
+    b_field_render_range = 1.0 #Multiple of coil radius
+    
+    def __init__(self, inductance=1.0, length=1.0, radius=1.0, pos=vec(0,0,0), orient=vec(0,0,1)):
+        self.mesh = helix(pos-0.5*orient
+        return
+    def update(self):
+        return
+    def set_inductance(self, inductance):
+        return
+    def calculate_b_field(self):
+        return
+
+class Capacitor:
+    def __init__(self):
+        return
+    
+class Resistor:
+    def __init__(self):
+        return
+    
+class Wire:
+    def __init__(self):
+        return
+
+
+def main():
+    pass
