ec3167479ee8673392c199bd4a3b0afe781f7263
VincentHuang25 <144478212+VincentHuang25@users.noreply.github.com>
2025-06-06 02:14:59 -0400

    runge kutta was fine but sliders had their issues fixed and started on reset

--- main.py
+++ main.py
@@ -1,303 +1,132 @@
-Web VPython 3.2
+Web VPython 3.2    
 
-COPPER = vec(184/255, 115/255, 51/255)
-ALUMINUM = vec(211/255, 211/255, 211/255)
-
-u_0 = 4*pi*10e-7
-e_0 = 8.85*10e-12
-WIRE_RADIUS = 0.02
-
-class Inductor:
-    b_field_render_range = 1.0 #Multiple of coil radius
-    b_field_density = 0.1
-    coil_down_factor = 0.1
-    coil = None
-    wire1 = None
-    wire2 = None
-    b_field = None
-    
-    def __init__(self, inductance=1.0, length=1.0, radius=1.0, wire_radius=0.01, pose=vec(0,0,0), orient=vec(0,1,0)):
-        self.inductance = inductance
-        self.length = length
-        self.radius = radius
-        self.pose = pose
-        self.orient = hat(orient)
-        self.wire_radius = wire_radius
-        self.render_coil()
-        return
-    
-    def get_n_coils(self, inductance, length, radius):
-        return round(sqrt((inductance*length)/(u_0*(radius**2)*pi))*self.coil_down_factor)
-        
-    def render_coil(self):
-        #delete old coil
-        if (self.coil != None):
-            self.coil.visible = False
-            
-        if (self.wire1 != None):
-            self.wire2.visible = False
-            
-        if (self.wire2 != None):
-            self.wire2.visible = False
-    
-        self.coil = helix()
-        self.coil.color = COPPER
-        self.coil.pos = self.pose-0.5*self.orient*self.length*0.7
-        self.coil.axis = self.orient
-        self.coil.radius = self.radius
-        self.coil.length = self.length*0.7
-        self.coil.thickness = self.wire_radius*2
-        self.coil.coils = self.get_n_coils(self.inductance, self.length, self.radius)
-        
-        self.wire1 = curve(
-            pos=[
-                self.pose-0.5*self.orient*self.length,
-                self.pose-0.5*self.orient*self.length*0.7, 
-                self.pose-0.5*self.orient*self.length*0.7+hat(self.coil.up)*self.coil.radius
-                ]
-            )
-        self.wire1.radius = self.coil.thickness/2
-        self.wire1.color = COPPER
-        self.wire1.rotate(axis=self.orient, angle=pi/2, origin=self.pose)
-        
-        self.wire2 = curve(
-            pos=[
-                self.pose+0.5*self.orient*self.length,
-                self.pose+0.5*self.orient*self.length*0.7, 
-                self.pose+0.5*self.orient*self.length*0.7+hat(self.coil.up)*self.coil.radius
-                ]
-            )
-        self.wire2.radius = self.coil.thickness/2
-        self.wire2.color = COPPER
-        self.wire2.rotate(axis=self.orient, angle=pi/2, origin=self.pose)
-        
-        return
-    
-    def render_b_field(self, current):
-        #delete old b_fields
-        if (self.b_field != None):
-            self.b_field.visible = False
-            
-        self.b_field = arrow(
-                pos=self.pose-self.orient*0.5*self.coil.length*current*0.05, 
-                axis=self.orient*self.coil.length*current*0.05,
-                color = color.blue,
-                round=True, 
-                headwidth=log(1+abs(0.015*current)),
-                shaftwidth=log(1+abs(0.005*current)),
-                )
-        
-        return
-    
-
-class Capacitor:
-    plate1 = None
-    plate2 = None
-    wire1 = None
-    wire2 = None
-    
-    plate_area_factor = 1e-9
-    plate_thickness = 0.01
-    wire_thickness = 0.25
-    
-    dist = 0.05
-    
-    e_fields = []
-    e_field_density = 0.1
-    e_field_scale = 1e-2
-    
-    def __init__(self, capacitance=1.0, length=1.0, radius=1.0, pose=vec(0,0,0), orient=vec(0,1,0)):
-        self.pose = pose
-        self.radius = radius
-        self.orient = hat(orient)
-        self.length = length
-        self.wire_len = length - (self.plate_thickness*2 + self.dist)/2
-        self.capacitance = capacitance
-        self.render_plates()
-        return
-    
-    def render_plates(self):
-        if (self.plate1 != None):
-            self.plate1.visible = False
-            
-        if (self.plate2 != None):
-            self.plate2.visible = False
-            
-        if (self.wire1 != None):
-            self.wire1.visible = False
-        
-        if (self.wire2 != None):
-            self.wire2.visible = False
-        
-        side_len = sqrt(self.capacitance*self.dist/e_0 * self.plate_area_factor)*1.1
-        
-        self.plate1 = box(
-            pos=self.pose+0.5*self.orient*self.dist, 
-            height=self.plate_thickness, length=side_len, width=side_len,
-            up = self.orient,
-            color = ALUMINUM
-            )
-            
-        self.plate2 = box(
-            pos=self.pose-0.5*self.orient*self.dist, 
-            height=self.plate_thickness, length=side_len, width=side_len,
-            up = -self.orient,
-            color = ALUMINUM
-            )
-     
-        self.wire1 = curve(
-            pos=[
-                self.pose+0.5*self.orient*(self.plate_thickness + self.dist),
-                self.pose+0.5*self.orient*(self.plate_thickness + self.dist + self.wire_len)
-                ],
-            radius=self.radius,
-            color=ALUMINUM
-            )
-            
-        self.wire2 = curve(
-            pos=[
-                self.pose-0.5*self.orient*(self.plate_thickness + self.dist),
-                self.pose-0.5*self.orient*(self.plate_thickness + self.dist + self.wire_len)
-                ],
-            radius=self.radius,
-            color=ALUMINUM
-            )
-            
-        return
-    
-    def render_e_fields(self, voltage):
-        if len(self.e_fields) > 0:
-            for e in self.e_fields:
-                e.visible = False
-            self.e_fields.clear()
-            
-        side_len = sqrt(self.capacitance*self.dist/e_0 * self.plate_area_factor)
-            
-        x_axis = norm(self.plate1.axis)
-        y_axis = norm(cross(x_axis, self.plate1.up))
-            
-        for x in range(-side_len/2, side_len/2+side_len*self.e_field_density, side_len*self.e_field_density):
-            for y in range(-side_len/2, side_len/2+side_len*self.e_field_density, side_len*self.e_field_density):
-                a = arrow(
-                    pos=self.pose-0.5*(voltage/abs(voltage))*self.orient*(self.dist-self.plate_thickness) + x_axis*x + y_axis*y,
-                    axis=(voltage/abs(voltage))*self.orient*(self.dist-self.plate_thickness),
-                    color=color.green,
-                    round=True,
-                    shaftwidth=(abs(voltage)/self.dist)*self.e_field_scale*0.001,
-                    headwidth=(abs(voltage)/self.dist)*self.e_field_scale*0.003,
-                    )
-                self.e_fields.append(a)
-                
-            
-
-class Resistor:
-    def __init__(self):
-        return
-    
-class Wire:
-    def __init__(self):
-        return
-
-
-def main():
-    c = Capacitor(capacitance=10e-6, radius=WIRE_RADIUS)
-    c.render_e_fields(5)
-
-if __name__ == "__main__":
-    main()
-
-
-R = 1
-L = 0.1
-C = 0.0001
-V = 1
-q = 0.5
-i = 0.5
+R = 10
+L = 10
+C = 10
+V = 10
+q = C*V
+i = 0
 dt = 0.000001
-t = 0     
+t = 0
+simulation_speed = 1000
+time_step = 1
 
 wtext(text="Vary the Resistance: \n")
-resistance_slider = slider( bind=resistance_set, min=0, max=50, step = 1, id = "resistance")
-wt2=wtext(text='{:1.2f}'.format(resistance_slider.value))
-scene.append_to_caption(' Ohms\n')
+resistance_slider = slider( bind=resistance_set, min=10, max=500, step = 1, id = "resistance")
+wt1=wtext(text='{:1.2f}'.format(resistance_slider.value))
+scene.append_to_caption(' ohms\n')
 wtext(text='<br>')
 
 wtext(text="Vary the Inductance: \n")
-inductance_slider = slider( bind=inductance_set, min=0, max=50, step = 1, id = "inductance")
-wt1=wtext(text='{:1.2f}'.format(inductance_slider.value))
-scene.append_to_caption(' Henrys\n')
+inductance_slider = slider( bind=inductance_set, min=10, max=1000, step = 1, id = "inductance")
+wt2=wtext(text='{:1.2f}'.format(inductance_slider.value))
+scene.append_to_caption(' millihenrys\n')
 wtext(text='<br>')
 
 wtext(text="Vary the Capacitance: \n")
-capacitance_slider = slider( bind=capacitance_set, min=0, max=50, step = 1, id = "capacitance")
+capacitance_slider = slider( bind=capacitance_set, min=10, max=1000, step = 1, id = "capacitance")
 wt3=wtext(text='{:1.2f}'.format(capacitance_slider.value))
-scene.append_to_caption(' Farads\n')
+scene.append_to_caption(' nanofarads\n')
 wtext(text='<br>')
 
 wtext(text="Vary the Capacitor Voltage: \n")
-capacitor_voltage_slider = slider( bind=capacitor_voltage_set, min=0, max=50, step = 1, id = "capacitor_voltage")
+capacitor_voltage_slider = slider( bind=capacitor_voltage_set, min=10, max=500, step = 1, id = "capacitor_voltage")
 wt4=wtext(text='{:1.2f}'.format(capacitor_voltage_slider.value))
-scene.append_to_caption(' Volts\n')
+scene.append_to_caption(' volts\n')
 wtext(text='<br>')
 
-wtext(text="Vary the Simulation Speed: \n", align="right")
-simulation_speed_slider = slider( bind=simulation_speed_set, min=0, max=50, step = 1, id = "simulation_speed")
+wtext(text="Vary the Simulation Speed: \n")
+simulation_speed_slider = slider( bind=simulation_speed_set, min=10, max=100, step = 1, id = "simulation_speed")
+wt5=wtext(text='{:1.2f}'.format(simulation_speed_slider.value))
+scene.append_to_caption(' \n')
 wtext(text='<br>')
 
 wtext(text="Vary the Time Step: \n")
-timestep_resolution_slider = slider( bind=timestep_resolution_set, min=0, max=50, step = 1, id = "timestep_resolution")
+time_step_slider = slider( bind=time_step_set, min=1, max=100, step = 1, id = "time_step")
+wt6=wtext(text='{:1.2f}'.format(time_step_slider.value))
+scene.append_to_caption(' microseconds\n')
 wtext(text='<br>')
 
 def resistance_set(evt):
-    wt2.text = '{:1.2f}'.format(resistance_slider.value)
+    global R
+    wt1.text = '{:1.2f}'.format(resistance_slider.value)
     if evt.id is "resistance":
         R = evt.value
 
 def inductance_set(evt):
-    console.log(evt)
-    wt1.text = '{:1.2f}'.format(inductance_slider.value)
+    global L
+    wt2.text = '{:1.2f}'.format(inductance_slider.value)
     if evt.id is "inductance":
-        L = evt.value
+        L = evt.value * 1e-3
 
 def capacitance_set(evt):
+    global C
     wt3.text = '{:1.2f}'.format(capacitance_slider.value)
     if evt.id is "capacitance":
-        C = evt.value
+        C = evt.value * 1e-9
 
 def capacitor_voltage_set(evt):
+    global V
+    global C
+    global q
     wt4.text = '{:1.2f}'.format(capacitor_voltage_slider.value)
     if evt.id is "capacitor_voltage":
         V = evt.value
+        q = C*V
 
 def simulation_speed_set(evt):
-    wt.text = '{:1.2f}'.format(simulation_speed_slider.value)
+    global simulation_speed
+    wt5.text = '{:1.2f}'.format(simulation_speed_slider.value)
     if evt.id is "simulation_speed":
-        self_simulation_speed = evt.value
+        simulation_speed = evt.value * 100
 
-def timestep_resolution_set(evt):
-    wt.text = '{:1.2f}'.format(timestep_resolution_slider.value)
-    if evt.id is "timestep_resolution":
-        self_timestep_resolution = evt.value
+def time_step_set(evt):
+    global dt
+    wt6.text = '{:1.2f}'.format(time_step_slider.value)
+    if evt.id is "time_step":
+        dt = evt.value * 1e-6
         
 running = False
 
-def Run(b):
+def Run(r):
+    global reset
+    reset = False
     global running
     running = not running
-    if running: b.text = "Pause"
-    else: b.text = "Run"
-button(text="Run", bind=Run)
+    if running: r.text = "Pause"
+    else: r.text = "Run"
 
-button(bind=reset, text="Reset")
+run = button(text="Run", bind=Run)
+
+wtext(text='<br>')
 wtext(text='<br>')
 
-def reset():
-    g1.delete()
-    g2.delete()
-    g3.delete()
-    g4.delete()
+reset = False
+
+def Reset():
+    run.text = "Run"
+    global running
     running = False
-    pass
+
+    global gc1,gc2,gc3,gc4,i2
+    
+    gc1.delete()
+    gc2.delete()
+    gc3.delete()
+    gc4.delete()
+
+    i2=0
+    
+    global R,L,C,V,q,i,dt,t
+    q = C*V
+    i = 0
+    dt = 0.000001
+    t = 0
+    global reset
+    reset = True
+    return
+
+button(bind=Reset, text="Reset")
 
 def f1(q,i):
     return i
@@ -320,28 +149,26 @@ def iRK4(q,i,dt):
     i += l
     return q,i
     
-g1 = graph(title='Current vs Time', xtitle='Time (s)', ytitle='Current (i)', align='right', width=400, height=300)
+g1 = graph(title='Current vs Time', xtitle='Time (s)', ytitle='Current (A)', width=400, height=300, align='left')
 gc1 = gcurve(color=color.red,graph=g1)
 
-g2 = graph(title='Voltage vs Time for Resistor', xtitle='Time (s)', ytitle='Voltage (i)', align='right', width=400, height=300)
+g2 = graph(title='Voltage vs Time for Resistor', xtitle='Time (s)', ytitle='Voltage (V)', width=400, height=300, align='left')
 gc2 = gcurve(color=color.green,graph=g2)
 
-g3 = graph(title='Voltage vs Time for Inductor', xtitle='Time (s)', ytitle='Voltage (i)', align='right', width=400, height=300)
+g3 = graph(title='Voltage vs Time for Inductor', xtitle='Time (s)', ytitle='Voltage (V)', width=400, height=300, align='left')
 gc3 = gcurve(color=color.blue,graph=g3)
 i2=0
 
-g4 = graph(title='Voltage vs Time for Capacitor', xtitle='Time (s)', ytitle='Voltage (i)', align='right', width=400, height=300)
+g4 = graph(title='Voltage vs Time for Capacitor', xtitle='Time (s)', ytitle='Voltage (V)', width=400, height=300, align='left')
 gc4 = gcurve(color=color.yellow,graph=g4)
-i3=0
 
-while True:
-    rate(1/dt)
+while not reset:
+    rate(simulation_speed)
     if running:
-        di2=i
-        i3+=i
+        i2=i
         q,i = iRK4(q,i,dt)
         gc1.plot(t,i)
         gc2.plot(t,i*R)
-        gc3.plot(t,(i-i2)/dt)
-        gc4.plot(t,i3)
+        gc3.plot(t,(i-i2)/dt*L)
+        gc4.plot(t,q/C)
         t += dt
